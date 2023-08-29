6523dbaa906a7b93af04300150a5a53fcee1132f
CodexAdrian <83074853+CodexAdrian@users.noreply.github.com>
2023-08-29 12:23:04 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   deploymentBranch: 'prod',
   trailingSlash: true,
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
